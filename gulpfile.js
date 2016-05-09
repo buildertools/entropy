507c5e23bfa89259a9b2dacfc9f168283370f68e
Jeff Nickoloff <jeff@allingeek.com>
2016-05-09 15:25:44 -0700

    Added help text on service restart.
    Signed-off-by: Jeff Nickoloff <jeff@allingeek.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var gulp  = require('gulp');
 var child = require('child_process');
 var server = null;
+var it = null;
 
 gulp.task('default', ['build', 'watch', 'spawn']);
 
 gulp.task('watch', function() {
-  gulp.watch('./**/*.go', ['build','spawn']);
+  gulp.watch('./**/*.go', ['build','printHelp','spawn']);
   gulp.watch('./**/*.go', doFmt)
   gulp.watch('./**/*.go', doVet)
 });
@@ -27,6 +28,10 @@ gulp.task('build', function() {
   return p;
 });
 
+gulp.task('printHelp', function() {
+  forkIt('helpOut', 'helpErr', 'entropy', [])
+});
+
 gulp.task('spawn', function() {
   if (server)
     server.kill();
@@ -38,6 +43,19 @@ gulp.task('spawn', function() {
     process.stdout.write(data.toString());
   });
 });
+
+function forkIt(outPrefix, errPrefix, bin, args) {
+  if (it)
+    it.kill();
+  it = child.spawn(bin, args);
+  it.stderr.on('data', function(data) {
+    process.stdout.write(errPrefix + data.toString());
+  });
+  it.stdout.on('data', function(data) {
+    process.stdout.write(outPrefix + data.toString());
+  });
+
+}
 
 function dumpStreams(outPrefix, errPrefix, p) {
   process.stdout.write(outPrefix + p.stdout.toString());
